255561b0f6ecfd230620f6d5d9aa17667db3c5d3
Bhavanamh <109859201+Bhavanamh@users.noreply.github.com>
2023-07-15 00:01:07 +0530

    Update One_dimentional_Array_inJava.java

--- One_dimentional_Array_inJava.java
+++ One_dimentional_Array_inJava.java
@@ -12,7 +12,7 @@ public class Creating_An_Array {
 		arr[2]=3;
 		arr[3]=4;
 		arr[4]=5;
-		//Printing array Elements
+		//Printing an array Elements
 		System.out.println("Array Elements are");
 		for(int i=0;i< arr.length;i++) {
 			System.out.print(arr[i]+" ");
@@ -79,5 +79,63 @@ public class Creating_An_Array {
 	}
 
 }
+
+
+	
+/*Remove even integer from an Array*/
+
+
+package one_Dimentional_Array;
+
+import java.util.Scanner;
+
+public class Remove_Even_Integer {
+
+	public static void main(String[] args) {
+		//array declaration and initialization
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Enter Number of Array Elements");
+				int n= sc.nextInt();
+				int[] arr= new int[n];
+				
+				System.out.println("Enter Array Elements");
+				for(int i=0;i< arr.length;i++) {
+					arr[i]=sc.nextInt();
+				}
+				sc.close();
+				print(arr);
+				int[] arr2=remove(arr);
+				System.out.println("After Removing Even Integer From An Array:");
+				print(arr2);
+	}
+	public static int[] remove(int arr[]) {
+		int count=0;
+		for(int i=0;i< arr.length;i++) {
+			if(arr[i]%2!=0) {
+				count++;
+			}
+		}
+		int[] arr1=new int[count];
+		//Removing even integer
+		int j=0;
+		for(int i=0;i< arr.length;i++) {
+			if(arr[i]%2!=0) {
+			arr1[j]=arr[i];
+			j++;
+			}
+		}
+		return arr1;
+	}
+	public static void print(int arr[]) {
+		//Printing array Elements
+		System.out.println("Array Elements are");
+		for(int i=0;i< arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+
+}
+
 
 
